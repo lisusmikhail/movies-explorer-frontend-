ef4877c712b918250bd4e73ccacf4d941d36a944
Mikhail Lisus <lisusmikhail@gmail.com>
2021-02-16 17:25:07 -0500

    first draft

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -3,9 +3,35 @@ import './AboutProject.css';
 
 function AboutProject() {
   return (
-    <div className='about-project'>
-
-    </div>
+    <section className='about'>
+      <h2 className='about__title section-title'>О проекте</h2>
+      <div className='about__content'>
+        <div className='about__description'>
+          <h3 className='about__description-title'>
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className='about__description-text'>
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </div>
+        <div className='about__description'>
+          <h3 className='about__description-title'>
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className='about__description-text'>
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </div>
+      </div>
+      <div className='about__gantt-chart'>
+        <div className='about__backend-chart'>1 неделя</div>
+        <div className='about__frontend-chart'>4 недели</div>
+        <div className='about__chart-label'>back-end</div>
+        <div className='about__chart-label'>front-end</div>
+      </div>
+    </section>
   );
 }
 
