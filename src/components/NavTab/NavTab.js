61605fc3a428284e3d898ce3291bb836eb68fed6
Mikhail Lisus <lisusmikhail@gmail.com>
2021-02-23 14:52:01 -0500

    refactor nav-tab

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -4,7 +4,7 @@ import './NavTab.css';
 
 function NavTab() {
   return (
-    <div className='nav-tab'>
+    <div className='nav-tab nav-tab_position'>
       <Link className='nav-tab__button' to='about' spy={true} smooth={true}>
         Узнать больше
       </Link>
